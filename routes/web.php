fa56c41752a4665eace15695a81f578d75daa895
milorad996 <93398405+milorad996@users.noreply.github.com>
2025-11-24 22:36:19 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/check-storage-link', function () {
-    $linkPath = public_path('storage');
-    if (file_exists($linkPath) && is_link($linkPath)) {
-        return 'Storage link postoji i pokazuje na: ' . readlink($linkPath);
-    } else {
-        return 'Storage link ne postoji!';
-    }
-});
-
-Route::get('/check-image', function () {
-    $filePath = storage_path('app/public/listings/gbN95irAd7vps40s5iq5IwWeb7VK3NwHTbY9vO6o.jpg');
-    if (file_exists($filePath)) {
-        return response()->file($filePath);
-    } else {
-        return 'Slika ne postoji na putanji: ' . $filePath;
-    }
-});
 
